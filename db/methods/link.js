3f817190852b97d90faf05cbd98993a452abc0fc
Pedro Correia <pedro.correia.105@gmail.com>
2018-07-14 16:42:56 +0100

    get link

--- db/methods/link.js
+++ db/methods/link.js
@@ -1,6 +1,12 @@
+
 let path = require('path')
 let Link = require(path.join(__dirname, '..', 'models', 'link'))
 
 module.exports.delete = async (companyId, edition) => {
   return Link.findOneAndRemove({ companyId: companyId, edition: edition })
 }
+
+module.exports.find = async (companyId, edition) => {
+    return Link.findOne({ companyId: companyId, edition: edition })
+}
+
--- routes/link.js
+++ routes/link.js
@@ -4,6 +4,38 @@ const Joi = require('joi')
 
 module.exports = [
   {
+    // link?companyId=...&edition=...
+    method: 'GET',
+    path: '/link',
+    config: {
+      tags: ['api'],
+      description: 'Gets a company link based on the company id and edition',
+      notes: 'Returns the link',
+      handler: async (request, h) => {
+        let companyId = request.query.companyId
+        let edition = request.query.edition
+
+        try {
+          let link = await request.server.methods.link.get(companyId, edition)
+          return link === null ? Boom.badData('No link associated') : link
+        } catch (err) {
+          logger.error(err)
+          return Boom.boomify(err)
+        }
+      },
+      validate: {
+        query: {
+          company: Joi.string().min(1)
+          .max(30).required()
+          .description('ID of company'),
+          edition: Joi.string().min(1)
+            .max(30).required()
+            .description('Edition Number')
+        },
+      },
+    }
+  },
+  {
     method: 'DELETE',
     path: '/link/company/{companyId}/edition/{edition}',
     config: {
