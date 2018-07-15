1d7230a038a74e6ec3f24ecd4ad3450c3a86f2c2
fchamicapereira <fchamicapereira@gmail.com>
2018-07-15 15:33:25 +0100

    completed tests for delete link

--- test/link.js
+++ test/link.js
@@ -1,16 +1,65 @@
 const path = require('path')
 const { before, after, it, describe } = require('mocha')
 const {expect} = require('chai')
+const Link = require(path.join('..', 'db', 'models', 'link'))
 let server
 
 describe('link', function () {
-  before('Starting server', function () {
+  before('Starting server', function (done) {
+    this.timeout(0)
     server = require(path.join(__dirname, '..', 'index'))
+
+    server.events.on('start', function () {
+      done()
+    })
   })
 
-  describe('test', function () {
-    it('should pass', function () {
-      expect(1).to.equal(1)
+  describe('delete', function () {
+    const newLinkData = {
+      companyId: 'someCompany',
+      edition: 'someEdition',
+      created: new Date(),
+      token: 'someToken',
+      valid: true,
+      participationDays: 3,
+      activities: [],
+      advertisementKind: 'someAdv'
+    }
+
+    before('adding link to db', async function () {
+      let newLink = new Link(newLinkData)
+
+      await newLink.save()
+    })
+
+    it('should be able to delete an existing link', async function () {
+      let response = await server.inject({
+        method: 'DELETE',
+        url: `/link/company/${newLinkData.companyId}/edition/${newLinkData.edition}`
+      })
+
+      let link = await Link.findOne(newLinkData)
+
+      expect(response.statusCode).to.eql(200)
+
+      Object.keys(newLinkData).forEach(key => {
+        expect(response.result[key]).to.eql(newLinkData[key])
+      })
+
+      expect(link).to.be.null
+    })
+
+    it('should give an error when trying to delete a nonexisting link', async function () {
+      let response = await server.inject({
+        method: 'DELETE',
+        url: `/link/company/${newLinkData.companyId}_nonexistent/edition/${newLinkData.edition}`
+      })
+
+      expect(response.statusCode).to.eql(422)
+    })
+
+    after('removing link to db', async function () {
+      await Link.findOneAndRemove(newLinkData)
     })
   })
 
