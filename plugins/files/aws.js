e4a2b44c323ca43aa22e2baaae23cfc1a4c8ce56
fchamicapereira <fchamicapereira@gmail.com>
2018-07-20 18:21:20 +0100

    completed logos endpoints

--- plugins/files/aws.js
+++ plugins/files/aws.js
@@ -2,6 +2,18 @@ const aws = require('aws-sdk')
 const path = require('path')
 const config = require(path.join(__dirname, '..', '..', 'config'))
 
+function promiseWrapper (s3, key, params, returnData) {
+  return new Promise((resolve, reject) => {
+    s3[key](params, (err, data) => {
+      if (err) {
+        reject(err)
+      }
+
+      resolve(returnData || data)
+    })
+  })
+}
+
 module.exports.download = (path, filename) => {
   let s3 = new aws.S3({
     endpoint: new aws.Endpoint(`https://${config.STORAGE.REGION}.${config.STORAGE.DOMAIN}${path}`),
@@ -9,39 +21,36 @@ module.exports.download = (path, filename) => {
     secretAccessKey: config.STORAGE.SECRET
   })
 
-  return new Promise((resolve, reject) => {
-    s3.getObject({
-      Bucket: config.STORAGE.NAME,
-      Key: filename
-    }, function (err, data) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
+  return promiseWrapper(s3, 'getObject', {
+    Bucket: config.STORAGE.NAME,
+    Key: filename
   })
 }
 
 module.exports.upload = (path, buffer, filename, isPublic) => {
+  const s3 = new aws.S3({
+    endpoint: new aws.Endpoint(`https://${config.STORAGE.REGION}.${config.STORAGE.DOMAIN}${path}`),
+    accessKeyId: config.STORAGE.KEY,
+    secretAccessKey: config.STORAGE.SECRET
+  })
+
+  return promiseWrapper(s3, 'putObject', {
+    ACL: isPublic !== undefined && isPublic ? 'public-read' : 'authenticated-read',
+    Body: buffer,
+    Bucket: config.STORAGE.NAME,
+    Key: filename
+  }, `https://${config.STORAGE.NAME}.${config.STORAGE.REGION}.${config.STORAGE.DOMAIN}${path}/${filename}`)
+}
+
+module.exports.delete = (path, filename) => {
   let s3 = new aws.S3({
     endpoint: new aws.Endpoint(`https://${config.STORAGE.REGION}.${config.STORAGE.DOMAIN}${path}`),
     accessKeyId: config.STORAGE.KEY,
     secretAccessKey: config.STORAGE.SECRET
   })
 
-  return new Promise((resolve, reject) => {
-    s3.putObject({
-      ACL: isPublic !== undefined && isPublic ? 'public-read' : 'authenticated-read',
-      Body: buffer,
-      Bucket: config.STORAGE.NAME,
-      Key: filename
-    }, function (err, data) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
+  return promiseWrapper(s3, 'deleteObject', {
+    Bucket: config.STORAGE.NAME,
+    Key: filename
   })
 }
--- plugins/files/index.js
+++ plugins/files/index.js
@@ -6,28 +6,35 @@ const ROOT = process.env.NODE_ENV === 'test' ? config.STORAGE.TEST : config.STOR
 
 async function uploadLogo (edition, companyId, file, filename) {
   const path = `${ROOT}${edition}/${companyId}/logos`
-  return aws.upload(path, file, filename, true)
+  try {
+    return await aws.upload(path, file, filename, true)
+  } catch (err) {
+    return null
+  }
 }
 
 async function downloadLogo (edition, companyId, filename) {
+  const path = `${ROOT}${edition}/${companyId}/logos`
+  try {
+    return await aws.download(path, filename)
+  } catch (err) {
+    return null
+  }
 }
 
 async function removeLogo (edition, companyId, filename) {
+  const path = `${ROOT}${edition}/${companyId}/logos`
+  try {
+    return await aws.delete(path, filename)
+  } catch (err) {
+    return null
+  }
 }
 
 module.exports = {
   name: 'files',
   version: '1.0.0',
   register: async (server, options) => {
-    let file = require('fs').readFileSync('./config.js')
-
-    try {
-      let result = await uploadLogo('myEdition', 'myCompany', file, 'newIndex.js')
-      console.log(result)
-    } catch (err) {
-      console.error(err)
-    }
-
     server.method('files.uploadLogo', uploadLogo)
     server.method('files.downloadLogo', downloadLogo)
     server.method('files.removeLogo', removeLogo)
