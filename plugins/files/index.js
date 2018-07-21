a431d35efc4c72a6d7752e11a7a71db7891d354b
fchamicapereira <fchamicapereira@gmail.com>
2018-07-21 03:12:57 +0100

    fixed a bug

--- plugins/files/index.js
+++ plugins/files/index.js
@@ -6,21 +6,27 @@ const ROOT = process.env.NODE_ENV === 'test' ? config.STORAGE.TEST : config.STOR
 
 function upload (dir, isPublic) {
   return (edition, companyId, file, filename) => {
-    const path = `${ROOT}${edition}/${companyId}/${dir}`
+    const path = dir === 'venue'
+      ? `${ROOT}${edition}/venue`
+      : `${ROOT}${edition}/${companyId}/${dir}`
     return aws.upload(path, file, filename, isPublic || false)
   }
 }
 
 function download (dir) {
   return (edition, companyId, filename) => {
-    const path = `${ROOT}${edition}/${companyId}/${dir}`
+    const path = dir === 'venue'
+      ? `${ROOT}${edition}/venue`
+      : `${ROOT}${edition}/${companyId}/${dir}`
     return aws.download(path, filename)
   }
 }
 
 function remove (dir) {
   return (edition, companyId, filename) => {
-    const path = `${ROOT}${edition}/${companyId}/${dir}`
+    const path = dir === 'venue'
+      ? `${ROOT}${edition}/venue`
+      : `${ROOT}${edition}/${companyId}/${dir}`
     return aws.delete(path, filename)
   }
 }
@@ -33,9 +39,9 @@ module.exports = {
     server.method('files.logo.download', download('logos'))
     server.method('files.logo.remove', remove('logos'))
 
-    server.method('files.venue.upload', upload('venues', true))
-    server.method('files.venue.download', download('venues'))
-    server.method('files.venue.remove', remove('venues'))
+    server.method('files.venue.upload', upload('venue', true))
+    server.method('files.venue.download', download('venue'))
+    server.method('files.venue.remove', remove('venue'))
 
     server.method('files.invoice.upload', upload('invoices', false))
     server.method('files.invoice.download', download('invoices'))
