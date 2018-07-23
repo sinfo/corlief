850319a40a2414277f43d499e1cb8df078c971d0
Miguel Araújo <miguel.f.araujo@tecnico.ulisboa.pt>
2018-07-23 23:33:43 +0100

    Added post endpoint

--- db/methods/link.js
+++ db/methods/link.js
@@ -1,6 +1,19 @@
 let path = require('path')
 let Link = require(path.join(__dirname, '..', 'models', 'link'))
 
+module.export.create = async (companyId, edition, token, participationDays, activities, advertisementKind) => {
+  return Link.create({
+    companyId: companyId,
+    edition: edition,
+    created: new Date(),
+    token: token,
+    valid: true,
+    participationDays: participationDays,
+    activities: activities,
+    advertisementKind: advertisementKind
+  })
+}
+
 module.exports.delete = async (companyId, edition) => {
   return Link.findOneAndRemove({ companyId: companyId, edition: edition })
 }
