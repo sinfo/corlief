9fdf0fe4eb98ce5bb1f7f3ccf93e7ef683d705b2
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2021-11-30 18:25:05 +0000

    removes test script

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -s --user 'api:key-7jm1c009ezjv85pkm1rqfxevufeovb43' \
-    https://api.mailgun.net/v3/sinfo.org/messages \
-    -F from='Excited User <mailgun@sinfo.org>' \
-    -F to=diogo.pacheco@sinfo.org \
-    -F subject='Hello' \
-    -F text='Testing some Mailgun awesomeness!'
